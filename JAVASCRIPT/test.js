be2f12b093da666e4996e2d0b07dd3cff239316c
Shaik Sonia <shaiksonu.2002@gmail.com>
2023-08-19 00:15:46 +0530

    javascript_basics

--- JAVASCRIPT/test.js
+++ JAVASCRIPT/test.js
@@ -8,3 +8,37 @@ let y;
 x = 5;
 y = 6;
 let z = x + y;
+
+
+
+// function declaration
+
+function sayHello(){
+    console.log("Hello");
+}
+sayHello();
+
+// function expression 
+var sayBye =  function(){
+    console.log("Bye");
+}
+
+sayBye();
+
+// what is the use of arguements()?
+// arguements allow us to not to repeat ourselvesand make our functions what we call more extensible  
+
+function sing(song){
+    console.log(song);
+}
+
+sing("helllllloooo");
+sing("hiiii");
+sing("byeeeeee")
+// as we can see it will help us to write less lines of code
+
+function multiply(a , b){
+
+    (a*b);
+}
+multiply(5,10);
